22f90ee6562a26d139965d5cbb853e55b43a38a7
Kalys Osmonov <kalys@osmonov.com>
2014-03-14 11:52:20 +0600

    Add tests for parser

---
+++ Project/nbkr-iosTests/NBKRXMLParserSpec.m
@@ -0,0 +1,30 @@
+//
+//  NBKRXMLParserSpec.m
+//  nbkr-ios
+//
+//  Created by Kalys Osmonov on 3/12/14.
+//  Copyright (c) 2014 Kalys Osmonov. All rights reserved.
+//
+
+#define EXP_SHORTHAND
+
+#import "Specta.h"
+#import "Expecta.h"
+#import "Nocilla.h"
+
+#import "NBKRXMLParser.h"
+
+
+SpecBegin(NBKRXMLParser)
+
+describe(@"parse", ^{
+    it(@"should parse data", ^{
+        NSString *dailyFixturesPath = [[NSBundle bundleForClass:[self class]] pathForResource:@"daily_valid_currencies" ofType:@"xml"];
+        NBKRXMLParser *parser = [[NBKRXMLParser alloc] initWithData:[NSData dataWithContentsOfFile:dailyFixturesPath]];
+        NSArray * rates = [parser parse];
+        expect(rates).notTo.beEmpty();
+        expect([[rates objectAtIndex:0] objectForKey:@"currency"]).to.equal(@"USD");
+    });
+});
+
+SpecEnd
