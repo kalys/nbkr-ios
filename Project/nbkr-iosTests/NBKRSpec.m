9a1947402ba6b8cfdee5d86aa53806311671d1ae
Kalys Osmonov <kalys@osmonov.com>
2014-03-14 14:59:07 +0600

    Reconsider main class usage

--- Project/nbkr-iosTests/NBKRSpec.m
+++ Project/nbkr-iosTests/NBKRSpec.m
@@ -33,18 +33,22 @@ describe(@"dailyCurrencyRates:error", ^{
     context(@"when response is OK", ^{
         it(@"should call successful block with data dictionary", ^AsyncBlock {
             NSString *dailyFixturesPath = [[NSBundle bundleForClass:[self class]] pathForResource:@"daily_valid_currencies" ofType:@"xml"];
+            NSData *expectedBody = [NSData dataWithContentsOfFile:dailyFixturesPath];
             stubRequest(@"GET", @"http://www.nbkr.kg/XML/daily.xml").
                 andReturn(200).
-                withBody([NSData dataWithContentsOfFile:dailyFixturesPath]);
+                withBody(expectedBody);
             NSArray *expectedResult = @[@"ololo"];
             id parser = [OCMockObject mockForClass:[NBKRXMLParser class]];
-            [(NBKRXMLParser *)[[parser stub] andReturn:expectedResult] parse:[OCMArg any]];
-            
-            [[[NBKR alloc] initWithParser:parser] dailyCurrencyRates:^(NSArray *rates) {
-                                            expect(rates).to.equal(expectedResult);
-                                            done();
-                                        }
-                                     error: nil
+            // [(NBKRXMLParser*) [parser expect] parse:expectedBody];
+            [(NBKRXMLParser *)[[parser stub] andReturn:expectedResult] parse:expectedBody];
+            NBKR *nbkr = [NBKR new];
+            nbkr.parser = parser;
+            [nbkr dailyCurrencyRates:^(NSArray *rates) {
+                    expect(rates).to.equal(expectedResult);
+                    [parser verify];
+                    done();
+                }
+                error: nil
             ];
         });
     });
@@ -54,7 +58,7 @@ describe(@"dailyCurrencyRates:error", ^{
             stubRequest(@"GET", @"http://www.nbkr.kg/XML/daily.xml").
                 andReturn(404);
             
-            [[[NBKR alloc] initWithParser:nil] dailyCurrencyRates:nil
+            [[NBKR new] dailyCurrencyRates:nil
                                      error:^(NSError *error) {
                                          NSLog(@"%@", error);
                                          done();
@@ -81,130 +85,26 @@ describe(@"weeklyCurrencyRates:error", ^{
     context(@"when response is OK", ^{
         it(@"should call successful block with data dictionary", ^AsyncBlock {
             NSString *weeklyFixturesPath = [[NSBundle bundleForClass:[self class]] pathForResource:@"weekly_valid_currencies" ofType:@"xml"];
+            NSData *expectedResponse = [NSData dataWithContentsOfFile:weeklyFixturesPath];
             stubRequest(@"GET", @"http://www.nbkr.kg/XML/weekly.xml").
-            andReturn(200).
-            withBody([NSData dataWithContentsOfFile:weeklyFixturesPath]);
+                andReturn(200).
+                withBody(expectedResponse);
+            
             NSArray *expectedResult = @[@"ololo"];
             id parser = [OCMockObject mockForClass:[NBKRXMLParser class]];
-            [(NBKRXMLParser *)[[parser stub] andReturn:expectedResult] parse:[OCMArg any]];
+            [(NBKRXMLParser *)[[parser stub] andReturn:expectedResult] parse:expectedResponse];
+            NBKR *nbkr = [NBKR new];
+            nbkr.parser = parser;
             
-            [[[NBKR alloc] initWithParser:parser] weeklyCurrencyRates:^(NSArray *rates) {
+            [nbkr weeklyCurrencyRates:^(NSArray *rates) {
                 expect(rates).to.equal(expectedResult);
+                [parser verify];
                 done();
             }
                                      error: nil
              ];
         });
     });
-    
-    context(@"when request is not OK", ^{
-        it(@"should call error block with error object", ^AsyncBlock {
-            stubRequest(@"GET", @"http://www.nbkr.kg/XML/weekly.xml").
-            andReturn(404);
-            
-            [[NBKR new] weeklyCurrencyRates:nil
-                                     error:^(NSError *error) {
-                                         NSLog(@"%@", error);
-                                         done();
-                                     }
-             ];
-        });
-    });
-    
-    context(@"when totally unknown error is occured", ^{
-        it(@"should call error block with error object", ^AsyncBlock {
-            stubRequest(@"GET", @"http://www.nbkr.kg/XML/weekly.xml").
-            andFailWithError([NSError errorWithDomain:@"foo" code:123 userInfo:nil]);
-            [[NBKR new] weeklyCurrencyRates:nil
-                                     error:^(NSError *error) {
-                                         NSLog(@"%@", error);
-                                         done();
-                                     }
-             ];
-        });
-    });
-
 });
 
-/*
-describe(@"currencyRates:error", ^{
-    context(@"when online", ^{
- 
-        beforeEach(^{
-            [NBKR resetInstance];
-            NSString *dailyFixturesPath = [[NSBundle bundleForClass:[self class]] pathForResource:@"daily_valid_currencies" ofType:@"xml"];
-            NSString *weeklyFixturesPath = [[NSBundle bundleForClass:[self class]] pathForResource:@"weekly_valid_currencies" ofType:@"xml"];
-            
-            stubRequest(@"GET", @"http://www.nbkr.kg/XML/daily.xml").
-                andReturn(200).
-                withBody([NSData dataWithContentsOfFile:dailyFixturesPath]);
-            
-            stubRequest(@"GET", @"http://www.nbkr.kg/XML/weekly.xml").
-                andReturn(200).
-                withBody([NSData dataWithContentsOfFile:weeklyFixturesPath]);
-        });
-        
-        it(@"should return list of currencies", ^AsyncBlock {
-            [[NBKR sharedInstance] currencyRates:^(NSDictionary *response){
-                expect(response).notTo.beEmpty();
-                
-                expect([response objectForKey:@"USD"]).to.beKindOf([NSDictionary class]);
-                expect([response objectForKey:@"RUB"]).to.beKindOf([NSDictionary class]);
-                
-                expect([[response objectForKey:@"USD"] objectForKey:@"rate"]).to.equal(@"29,2717");
-                expect([[response objectForKey:@"RUB"] objectForKey:@"rate"]).to.equal(@"1,4992");
-                
-                done();
-            }
-            error:nil];
-        });
-    });
- */
-    
-    /*
-     
-    TODO: v1.1
-     
-    context(@"when something wrong with internet or server is down", ^{
-        beforeEach(^{
-            
-            [NBKR resetInstance];
-            // successful requests
-            NSString *dailyFixturesPath = [[NSBundle bundleForClass:[self class]] pathForResource:@"daily_valid_currencies" ofType:@"xml"];
-            NSString *weeklyFixturesPath = [[NSBundle bundleForClass:[self class]] pathForResource:@"weekly_valid_currencies" ofType:@"xml"];
-            
-            stubRequest(@"GET", @"http://www.nbkr.kg/XML/daily.xml").
-            andReturn(200).
-            withBody([NSData dataWithContentsOfFile:dailyFixturesPath]);
-            
-            stubRequest(@"GET", @"http://www.nbkr.kg/XML/weekly.xml").
-            andReturn(200).
-            withBody([NSData dataWithContentsOfFile:weeklyFixturesPath]);
-            
-            [[NBKR sharedInstance] currencyRates:^(NSDictionary * response) {} error:nil];
-            
-            [NSThread sleepForTimeInterval:1.0];
-            [[LSNocilla sharedInstance] clearStubs];
-            [NBKR resetInstance];
-            
-            stubRequest(@"GET", @"http://www.nbkr.kg/XML/daily.xml").andReturn(503);
-            stubRequest(@"GET", @"http://www.nbkr.kg/XML/weekly.xml").andReturn(503);
-        });
-
-        it(@"should read data from file when there is no internet connection", ^AsyncBlock {
-            [[NBKR sharedInstance] currencyRates: ^(NSDictionary *response) {
-                expect(response).notTo.beEmpty();
-                expect([response objectForKey:@"USD"]).to.beKindOf([NSDictionary class]);
-                expect([response objectForKey:@"RUB"]).to.beKindOf([NSDictionary class]);
-                done();
-            }
-            error: nil];
-        });
-        
-        it(@"should call error block if there is old cache file", ^{});
-    });
-     */
-/*
-});
-*/
 SpecEnd
